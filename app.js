eff5ef8a252b79a19e02e3609affbbf97ceb9707
Augastin K Lazar <laugastin@gmail.com>
2023-07-04 12:43:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,17 +18,54 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  // Create a simple rotating cube
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  // Create an array to hold the cubes
+  const cubes = [];
 
-  // Animate the cube
+  // Create a function to generate random colors
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  // Create cubes with random colors
+  const cubeCount = 10;
+  for (let i = 0; i < cubeCount; i++) {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: getRandomColor() });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.set(
+      Math.random() * 10 - 5,
+      Math.random() * 10 - 5,
+      Math.random() * 10 - 5
+    );
+    cubes.push(cube);
+    scene.add(cube);
+  }
+
+  // Create code snippets in the background
+  const snippetCount = 50;
+  for (let i = 0; i < snippetCount; i++) {
+    const snippet = document.createElement('div');
+    snippet.className = 'code-snippet';
+    snippet.style.backgroundColor = getRandomColor();
+    snippet.style.left = Math.random() * window.innerWidth + 'px';
+    snippet.style.top = Math.random() * window.innerHeight + 'px';
+    document.body.appendChild(snippet);
+  }
+
+  // Animate the scene
   function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+
+    // Change the color of the cubes
+    cubes.forEach(cube => {
+      cube.material.color.set(getRandomColor());
+    });
+
     renderer.render(scene, camera);
   }
 
